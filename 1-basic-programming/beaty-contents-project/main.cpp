6e4cce9c94b2136c2ba9c70b3b999a019c26f360
eyub197 <eyubmehmed122@gmail.com>
2025-12-09 20:31:42 +0200

    forgot to save :)

--- 1-basic-programming/beaty-contents-project/main.cpp
+++ 1-basic-programming/beaty-contents-project/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 struct Contestant {
-
+    int number;
+    char name[12];
+    int age;
+    char gender[2];
+    float hipCircumference;
+    float shoulderCircumference;
+    float neckCircumference;
+    float calfCircumference;
 };
+
+const int MAX_NUM_OF_CONTESTANTS = 30;
 
 int main() {
 
